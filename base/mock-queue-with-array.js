995f6f330a593c13ccf87dcb7fe7443f6cf0f662
chenxianlong <794465731@qq.com>
2019-06-21 01:55:23 +0800

    day14-stack

--- base/mock-queue-with-array.js
+++ base/mock-queue-with-array.js
@@ -13,9 +13,11 @@ class ArrayQueue {
       if (this.head === 0) return false
 
       for (let i = this.head; i < this.tail; i++) {
+        // i - this.head
+        // think of this.head is offset
         this.items[i - this.head] = this.items[i]
       }
-      this.tail = this.head
+      this.tail -= this.head
       this.head = 0
     }
     this.items[this.tail] = item
@@ -32,3 +34,17 @@ class ArrayQueue {
     return current
   }
 }
+
+const arrQueue = new ArrayQueue(6)
+arrQueue.enqueue(1)
+arrQueue.enqueue(2)
+arrQueue.enqueue('c')
+arrQueue.enqueue('a')
+arrQueue.enqueue('b')
+arrQueue.enqueue('d')
+arrQueue.dequeue()
+arrQueue.dequeue()
+arrQueue.enqueue('x')
+console.log(arrQueue.items)
+console.log(arrQueue.tail)
+console.log(arrQueue.head)
